fb67d131c0e15fe71df80d6d9ba7f9e8a412be19
Linda Johnstone <linda.johnstone@myob.com>
2021-06-28 17:25:13 +1000

    Calculate BMI kata

---
+++ CalculateBMI.cs
@@ -0,0 +1,22 @@
+/*
+Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+*/
+public class CalculateBMI
+{
+    public static string Bmi(double weight, double height)
+    {
+        var bmi = weight / (height * height);
+        if (bmi <= 18.5) return "Underweight";
+        if (bmi <= 25.0) return "Normal";
+        if (bmi <= 30.0) return "Overweight";
+        return "Obese";
+    }
+}
