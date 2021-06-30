b9abd55799186d41b2440ce5488ec9e8eb38d8cd
Linda Johnstone <linda.johnstone@myob.com>
2021-06-30 17:37:53 +1000

    30/6

---
+++ HighestAndLowest.cs
@@ -0,0 +1,34 @@
+/* 
+    In this little assignment you are given a string of space separated numbers, and have to 
+    return the highest and lowest number.
+
+    Example:
+
+    Kata.HighAndLow("1 2 3 4 5");  // return "5 1"
+    Kata.HighAndLow("1 2 -3 4 5"); // return "5 -3"
+    Kata.HighAndLow("1 9 3 4 -5"); // return "9 -5"
+    Notes:
+
+    All numbers are valid Int32, no need to validate them.
+    There will always be at least one number in the input string.
+    Output string must be two numbers separated by a single space, and highest number is first.
+*/
+using System;
+
+public static class HighestAndLowest
+{
+    public static string HighAndLow(string numbers)
+    {
+        var array = numbers.Split(" ");
+        var nums = new int[array.Length];
+        for (int i = 0; i < array.Length; i++)
+        {
+            nums[i] = Int32.Parse(array[i]);
+        }
+        Array.Sort(nums);
+        var min = nums[0].ToString();
+        var max = nums[nums.Length - 1].ToString();
+
+        return max + " " + min;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,11 +7,18 @@ namespace CodeWars
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine(OverTheRoad.overTheRoad(1, 3)); // 6
-            Console.WriteLine(OverTheRoad.overTheRoad(6, 3)); // 1
-            Console.WriteLine(OverTheRoad.overTheRoad(3, 3)); // 4
-            Console.WriteLine(OverTheRoad.overTheRoad(2, 3)); // 5
-            Console.WriteLine(OverTheRoad.overTheRoad(3, 5)); // 8
+            Console.WriteLine(HighestAndLowest.HighAndLow("1 2 3 4 5"));  // return "5 1"
+            Console.WriteLine(HighestAndLowest.HighAndLow("1 2 -3 4 5")); // return "5 -3"
+            Console.WriteLine(HighestAndLowest.HighAndLow("1 9 3 4 -5")); // return "9 -5"
+            // var countByOnes = CountByX.CountBy(1, 10);
+            // Console.WriteLine(String.Join(", ", countByOnes));
+            // var countByTwos = CountByX.CountBy(2, 5);
+            // Console.WriteLine(String.Join(", ", countByTwos));
+            // Console.WriteLine(OverTheRoad.overTheRoad(1, 3)); // 6
+            // Console.WriteLine(OverTheRoad.overTheRoad(6, 3)); // 1
+            // Console.WriteLine(OverTheRoad.overTheRoad(3, 3)); // 4
+            // Console.WriteLine(OverTheRoad.overTheRoad(2, 3)); // 5
+            // Console.WriteLine(OverTheRoad.overTheRoad(3, 5)); // 8
             // Console.WriteLine(ReturningStrings.Greet("Linda"));
             // Console.WriteLine(ReturnNegative.MakeNegative(0));
             // Console.WriteLine(RemoveFirstAndLastCharacters.Remove_char("apple"));
