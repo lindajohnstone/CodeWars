64af20691e5e551b88d75544516999a7dcc841a0
Linda Johnstone <linda.johnstone@myob.com>
2021-07-20 11:46:42 +1000

    20/7

--- FindOddInt.cs
+++ FindOddInt.cs
@@ -1,5 +1,7 @@
 namespace CodeWars
 {
+    using System;
+    using System.Collections.Generic;
     // Given an array of integers, find the one that appears an odd number of times.
 
     // There will always be only one integer that appears an odd number of times.
@@ -10,8 +12,13 @@ namespace CodeWars
         {
             var numbers = seq.ToList();
             var count = 0;
-            // work out logic
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                var numCount = numbers.FindAll(x => x == numbers[i]).Count % 2 == 1;
+                count = numbers[i];
+            }
             return count;
+            //return seq.GroupBy(x => x).Single(g => g.Count() % 2 == 1).Key;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace CodeWars
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine(FindOddInt.find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 })); // returns 5
+            Console.WriteLine(FindOddInt.find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 })); // returns 5 // solution returns -1
             // var array1 = ArrayDifference.ArrayDiff(new int[] { 1, 2 }, new int[] { 1 });
             // foreach (var number in array1)
             // {
