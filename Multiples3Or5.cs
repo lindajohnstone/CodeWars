9f72307efb367b2afa1c98e63288a70ba3af3b8d
Linda Johnstone <linda.johnstone@myob.com>
2021-08-12 09:54:09 +1000

    Return to original code

--- Multiples3Or5.cs
+++ Multiples3Or5.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 namespace CodeWars
 {
-    //     If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+    // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
     // Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 
@@ -12,8 +12,15 @@ namespace CodeWars
     {
         public static int Solution(int value)
         {
-            if (value <= 0) return 0;
-            return Enumerable.Range(0, value - 1).Where(x => x % 3 == 0 || x % 5 == 0).Sum();
+            var multiplesList = new List<int>();
+            for (var i = 1; i < value; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0 &&!multiplesList.Contains(i)) multiplesList.Add(i);
+            }
+            var sum = multiplesList.Sum();
+            return sum;
+            // if (value <= 0) return 0;
+            // return Enumerable.Range(0, value - 1).Where(x => x % 3 == 0 || x % 5 == 0).Sum();
         }
     }
 }
